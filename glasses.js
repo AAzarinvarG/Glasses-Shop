41c049d7787b698ea0c51436b4ac3960c2af1c8e
abolfazl <abolfazl@gmail.com>
2023-04-04 17:57:53 +0430

    stage animation to place text * description & button

---
+++ glasses.js
@@ -0,0 +1,11 @@
+const $ = document;
+
+let placeTextDescBtn = $.querySelector('#place-text-desc-btn');
+
+window.addEventListener('load', () => {
+    
+    setTimeout(() => {
+        placeTextDescBtn.style.marginTop = '0';
+    }, 2000);
+    
+});
