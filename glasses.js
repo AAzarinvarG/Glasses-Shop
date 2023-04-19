9d7ea143cf062512c47ef10c2afc68f74969ef6a
abolfazl <abolfazl@gmail.com>
2023-04-20 02:29:12 +0430

    update price

--- glasses.js
+++ glasses.js
@@ -32,27 +32,27 @@ window.addEventListener('load', () => {
 });
 
 let versaceGlassesArray = [
-    { img: 'images/glassesVersace1.avif', price: '230$' },
-    { img: 'images/glassesVersace2.avif', price: '910$' },
-    { img: 'images/glassesVersace3.avif', price: '460$' },
-    { img: 'images/glassesVersace4.avif', price: '632$' },
-    { img: 'images/glassesVersace5.avif', price: '110$' },
+    { img: 'images/glassesVersace1.avif', price: '230' },
+    { img: 'images/glassesVersace2.avif', price: '910' },
+    { img: 'images/glassesVersace3.avif', price: '460' },
+    { img: 'images/glassesVersace4.avif', price: '632' },
+    { img: 'images/glassesVersace5.avif', price: '110' },
 ]
 
 let diorGlassesArray = [
-    { img: 'images/glassesDior1.avif', price: '411$' },
-    { img: 'images/glassesDior2.avif', price: '110$' },
-    { img: 'images/glassesDior3.webp', price: '506$' },
-    { img: 'images/glassesDior4.avif', price: '890$' },
-    { img: 'images/glassesDior5.webp', price: '230$' },
+    { img: 'images/glassesDior1.avif', price: '411' },
+    { img: 'images/glassesDior2.avif', price: '110' },
+    { img: 'images/glassesDior3.webp', price: '506' },
+    { img: 'images/glassesDior4.avif', price: '890' },
+    { img: 'images/glassesDior5.webp', price: '230' },
 ]
 
 let pradaGlassesArray = [
-    { img: 'images/glassesPrada1.jpg', price: '349$' },
-    { img: 'images/glassesPrada2.jpg', price: '890$' },
-    { img: 'images/glassesPrada3.webp', price: '315$' },
-    { img: 'images/glassesPrada4.avif', price: '670$' },
-    { img: 'images/glassesPrada5.avif', price: '937$' },
+    { img: 'images/glassesPrada1.jpg', price: '349' },
+    { img: 'images/glassesPrada2.jpg', price: '890' },
+    { img: 'images/glassesPrada3.webp', price: '315' },
+    { img: 'images/glassesPrada4.avif', price: '670' },
+    { img: 'images/glassesPrada5.avif', price: '937' },
 ]
 
 versaceGlassesArray.forEach((item) => {
@@ -65,12 +65,11 @@ versaceGlassesArray.forEach((item) => {
                                                                     Beautiful colored Versace glasses with excellent quality
                                                                     You can buy it from the button below
                                                                 </p>
-                                                                <p id="glasses-versace-price"> ${item.price} </p>
+                                                                <p id="glasses-versace-price"> $${item.price} </p>
                                                                 <button id="btn-add-to-basket-versace"> add to basket </button>
                                                            </div> `);
 
 });
-
 
 diorGlassesArray.forEach((item) => {
 
@@ -82,7 +81,7 @@ diorGlassesArray.forEach((item) => {
                                                                 Beautiful colored Versace glasses with excellent quality
                                                                 You can buy it from the button below
                                                             </p>
-                                                            <p id="glasses-dior-price"> ${item.price} </p>
+                                                            <p id="glasses-dior-price"> $${item.price} </p>
                                                             <button id="btn-add-to-basket-dior"> add to basket </button>
                                                        </div>`);
 
@@ -98,7 +97,7 @@ pradaGlassesArray.forEach((item) => {
                                                                 Beautiful colored Versace glasses with excellent quality
                                                                 You can buy it from the button below
                                                             </p>
-                                                            <p id="glasses-prada-price"> ${item.price} </p>
+                                                            <p id="glasses-prada-price"> $${item.price} </p>
                                                             <button id="btn-add-to-basket-prada"> add to basket </button>
                                                         </div>`);
 
