2aee5dba08f6cf40513f20b89b7518902886a4aa
abolfazl <abolfazl@gmail.com>
2023-04-30 19:14:11 +0430

    add number for Array and set event change on input in glasses in bag

--- glasses.js
+++ glasses.js
@@ -67,27 +67,27 @@ window.addEventListener('load', () => {
 });
 
 let versaceGlassesArray = [
-    { img: 'images/glassesVersace1.avif', price: 230, id: 1, name: 'Versace Glasses' },
-    { img: 'images/glassesVersace2.avif', price: 910, id: 2, name: 'Versace Glasses' },
-    { img: 'images/glassesVersace3.avif', price: 460, id: 3, name: 'Versace Glasses' },
-    { img: 'images/glassesVersace4.avif', price: 632, id: 4, name: 'Versace Glasses' },
-    { img: 'images/glassesVersace5.avif', price: 110, id: 5, name: 'Versace Glasses' },
+    { img: 'images/glassesVersace1.avif', price: 230, id: 1, name: 'Versace Glasses', number: 1 },
+    { img: 'images/glassesVersace2.avif', price: 910, id: 2, name: 'Versace Glasses', number: 1 },
+    { img: 'images/glassesVersace3.avif', price: 460, id: 3, name: 'Versace Glasses', number: 1 },
+    { img: 'images/glassesVersace4.avif', price: 632, id: 4, name: 'Versace Glasses', number: 1 },
+    { img: 'images/glassesVersace5.avif', price: 110, id: 5, name: 'Versace Glasses', number: 1 },
 ]
 
 let diorGlassesArray = [
-    { img: 'images/glassesDior1.avif', price: 411, id: 1, name: 'Dior Glasses' },
-    { img: 'images/glassesDior2.avif', price: 110, id: 2, name: 'Dior Glasses' },
-    { img: 'images/glassesDior3.webp', price: 506, id: 3, name: 'Dior Glasses' },
-    { img: 'images/glassesDior4.avif', price: 890, id: 4, name: 'Dior Glasses' },
-    { img: 'images/glassesDior5.webp', price: 225, id: 5, name: 'Dior Glasses' },
+    { img: 'images/glassesDior1.avif', price: 411, id: 1, name: 'Dior Glasses', number: 1 },
+    { img: 'images/glassesDior2.avif', price: 110, id: 2, name: 'Dior Glasses', number: 1 },
+    { img: 'images/glassesDior3.webp', price: 506, id: 3, name: 'Dior Glasses', number: 1 },
+    { img: 'images/glassesDior4.avif', price: 890, id: 4, name: 'Dior Glasses', number: 1 },
+    { img: 'images/glassesDior5.webp', price: 225, id: 5, name: 'Dior Glasses', number: 1 },
 ]
 
 let pradaGlassesArray = [
-    { img: 'images/glassesPrada1.jpg', price: 349, id: 1, name: 'Prada Glasses' },
-    { img: 'images/glassesPrada2.jpg', price: 898, id: 2, name: 'Prada Glasses' },
-    { img: 'images/glassesPrada3.webp', price: 315, id: 3, name: 'Prada Glasses' },
-    { img: 'images/glassesPrada4.avif', price: 670, id: 4, name: 'Prada Glasses' },
-    { img: 'images/glassesPrada5.avif', price: 937, id: 5, name: 'Prada Glasses' },
+    { img: 'images/glassesPrada1.jpg', price: 349, id: 1, name: 'Prada Glasses', number: 1 },
+    { img: 'images/glassesPrada2.jpg', price: 898, id: 2, name: 'Prada Glasses', number: 1 },
+    { img: 'images/glassesPrada3.webp', price: 315, id: 3, name: 'Prada Glasses', number: 1 },
+    { img: 'images/glassesPrada4.avif', price: 670, id: 4, name: 'Prada Glasses', number: 1 },
+    { img: 'images/glassesPrada5.avif', price: 937, id: 5, name: 'Prada Glasses', number: 1 },
 ]
 
 versaceGlassesArray.forEach((item) => {
@@ -124,9 +124,9 @@ function GlassesaddToBagArray(Id, ArrayName) {
 function priceCalculation() {
     let totalPrice = bagArray.reduce((prev, next) => {
         return prev + next.price;
-   }, 0);
+    }, 0);
 
-   totalPriceElemInBag.innerHTML = `$${totalPrice}`;
+    totalPriceElemInBag.innerHTML = `$${totalPrice}`;
 }
 
 function addToBag() {
@@ -139,11 +139,12 @@ function addToBag() {
                                                                 </div>
                                                                 <div id="header-name-input-price-removeIcon-in-bag">
                                                                     <p id="name-glasses-in-bag"> ${item.name} </p>
-                                                                    <input type="number" value="1" min="1" max="3">
+                                                                    <input id="tes" onchange="change(event.target.value, ${item.id})" type="number" value="1" min="1" max="3">
                                                                     <ion-icon onclick="removeGlasses(${item.id})" name="trash-outline"></ion-icon>
                                                                     <p id="price-in-glasses-in-bag"> $${item.price} </p>
                                                                 </div>
                                                             </div>`);
+
     });
 
 }
@@ -152,7 +153,7 @@ function removeGlasses(Id) {
     let findIndex = bagArray.findIndex((item) => {
         return item.id == Id;
     });
-    
+
     bagArray.splice(findIndex, 1);
     addToBag();
     priceCalculation();
